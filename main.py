e15c148fb683967cacd249f6bd465964b1152f76
pclark5704 <pclark@pclark.com>
2024-05-30 05:26:09 -0400

    Final version of code

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
-    print(chars_dict)
-
+    print("--- Begin report of",book_path," ---")
+    print("There were",num_words,"words found in the document\n\n")
+    character_times(chars_dict)
 
 def get_num_words(text):
     words = text.split()
     return len(words)
-
 
 def get_chars_dict(text):
     chars = {}
@@ -21,10 +21,14 @@ def get_chars_dict(text):
             chars[lowered] = 1
     return chars
 
-
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+def character_times(counter_dict):
+    sorted_dict = dict(sorted(counter_dict.items(), reverse=True, key=lambda item:item[1]))
+    for character in sorted_dict:
+        if character.isalpha() == True:
+            print("The character",character,"occurs",sorted_dict[character],"times.")
 
 main()
